f89af93c6310fa833be1d5eebee9778a56623c83
vannguyenv12 <npnv.vn1@gmail.com>
2024-07-10 19:43:20 +0700

    Core: Success Response

--- src/controllers/access.controller.ts
+++ src/controllers/access.controller.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from "express";
+import { CREATED } from "~/core/success.error";
 import AccessService from "~/services/access.service";
 
 class AccessController {
   public signUp = async (req: Request, res: Response, next: NextFunction) => {
-    return res.status(201).json(await AccessService.signUp(req.body))
+    new CREATED({
+      message: 'Registered Ok!',
+      metadata: await AccessService.signUp(req.body)
+    }).send(res);
 
   }
 }
---
+++ src/core/success.error.ts
@@ -0,0 +1,43 @@
+import { Response } from "express";
+
+const StatusCode = {
+  OK: 200,
+  CREATED: 201,
+}
+
+const ReasonStatusCode = {
+  OK: 'Success',
+  CREATED: 'Created',
+}
+
+
+class SuccessResponse {
+  private message: string;
+  private statusCode: number;
+  private metadata: any;
+
+  constructor({ message = '', statusCode = StatusCode.OK, reasonStatusCode = ReasonStatusCode.OK, metadata = {} }) {
+    this.message = !message ? reasonStatusCode : message;
+    this.statusCode = statusCode;
+    this.metadata = metadata;
+  }
+
+  send(res: Response, headers = {}) {
+    return res.status(this.statusCode).json(this);
+  }
+}
+
+export class OK extends SuccessResponse {
+  constructor({ message = '', metadata = {} }) {
+    super({ message, metadata })
+  }
+}
+
+export class CREATED extends SuccessResponse {
+  private options: any;
+
+  constructor({ message = '', statusCode = StatusCode.CREATED, reasonStatusCode = ReasonStatusCode.CREATED, metadata = {}, options = {} }) {
+    super({ message, statusCode, reasonStatusCode, metadata })
+    this.options = options
+  }
+}
