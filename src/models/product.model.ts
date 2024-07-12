90f95ebe5bad0ffc857bffa1d2ce89ee5ccd26b0
vannguyenv12 <npnv.vn1@gmail.com>
2024-07-12 13:44:39 +0700

    model: Product

---
+++ src/models/product.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const DOCUMENT_NAME = 'Product';
+const COLLECTION_NAME = 'Products';
+
+const productSchema = new mongoose.Schema({
+  product_name: { type: String, required: true },
+  product_thumb: { type: String, required: true },
+  product_description: String,
+  product_price: { type: Number, required: true },
+  product_quantity: { type: Number, required: true },
+  product_type: { type: String, required: true, enum: ['Electronics', 'Clothing', 'Furniture'] },
+  product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
+  product_attribute: { type: Schema.Types.Mixed, required: true },
+}, {
+  collection: COLLECTION_NAME,
+  timestamps: true,
+});
+
+// Define product type
+const clothingSchema = new Schema({
+  brand: { type: String, required: true },
+  size: String,
+  material: String
+}, {
+  collection: 'clothes',
+  timestamps: true
+})
+
+// Define product electronic
+const electronicSchema = new Schema({
+  manufacturer: { type: String, required: true },
+  model: String,
+  color: String
+}, {
+  collection: 'electronics',
+  timestamps: true
+})
+
+//Export the model
+export const productModel = mongoose.model(DOCUMENT_NAME, productSchema);
+export const clothingModel = mongoose.model('Clothing', clothingSchema);
+export const electronicModel = mongoose.model('Electronics', electronicSchema);
